062728a2aaacadc76044e2c7ef3f07b7eb2d174d
adhi <adhirajsingh7303@gmail.com>
2021-04-13 12:34:37 +0530

    a

---
+++ js-budget-setup-master/javascript/js/app.js
@@ -0,0 +1,46 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+  newMethod(){
+   const value=this.budgetInput.value;
+   if(value < 0){
+     this.budgetFeedback.classList.add('showitem');
+     this.budgetFeedback.innerHTML='<p>plesae add correct input</p>';
+   }else{
+     this.budgetAmount.textContent=value;
+   }
+}
+}
+function eventListeners(){
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+    const ui=new UI(); 
+
+  budgetForm.addEventListener('submit',function(event){
+     event.preventDefault();
+     ui.newMethod();
+  })
+  expenseForm.addEventListener('submit',function(event){
+    event.preventDefault();
+  })
+  expenseList.addEventListener('click',function(){
+    
+  })
+}
+document.addEventListener('DOMContentLoaded',eventListeners)
